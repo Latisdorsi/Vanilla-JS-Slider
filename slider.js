9a69a27cc099d6ea4a256bbe1a4f744d1869e721
Christopher Cuna <chrohn@outlook.com>
2015-08-25 00:30:12 +0800

    first commit

---
+++ slider.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+	var slider = e('slider'), //Slider
+	sliderItems = getElementsByTagName('img'),  //Array of Slider Items
+	noOfSliderItems = sliderItems.length, 
+	move;
+
+
+}
