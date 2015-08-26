89c0d7a3c4adadf8c419e4413719a0d0d0aa6f24
Christopher Cuna <chrohn@outlook.com>
2015-08-26 21:27:17 +0800

    Button and opacity changes need to be added

--- slider.js
+++ slider.js
@@ -1,31 +1,44 @@
-window.onload = function(){
-	
-	function e(eel) { //eel = element in question when someone uses e('ElementName'). You can use anything here
-    return document.getElementById(eel); //The above e can be changed too, maybe to a dollar sign?
-	}
-	
-	var slider = e('slider'), //Slider
-	var sliderContent = e('slider-content')
-	sliderItems = slider.getElementsByTagName('img'),  //Array of Slider Items
-	noOfSliderItems = sliderItems.length, 
-	move, n = 0, //Determines The Slide No.
-	imgWidth = (100 / noOfSliderItems) + "%";
-	
+window.onload = function() {
+    //Slider Module
 
-	function sliderInit() {
-    sliderContent.style.width = (noOfImages * 100) + "%"; //Adds 'width' using inline CSS.
-    	for (var i = 0; i < noOfImages; i++) { // Loops through all available images
-        	sliderImages[i].style.width = imgWidth; // Sets imgWidth as the width of each image in the slider
-    	}
-	}
+    var slider = (function() {
+        //CacheDom
+        var slider = document.getElementById('carousel'), //Slider
+            sliderContent = document.getElementsByTagName('ul')[0], //The First Array Within The Document
+	        sliderImages = slider.getElementsByTagName('li');
+        //Declare Variables
+        var noOfSliderItems = sliderImages.length,
+            n = 0, //Determines The Slide No.
+            imgWidth = (100 / noOfSliderItems) + "%";
+	        fps = 15, //Sets Interval Time For Animation
+			startTime = null;
 
-	/*
-	move = setInverval(function () {
-		if (n < (noOfImages - 1) ) n++;
-		else n = 0;
-		
-	}
+        _render();
+        setAnimation();
+        function _render() {
+            sliderContent.style.width = (noOfSliderItems * 100) + "%"; //Adds 'width' using inline CSS.
+            for (var i = 0; i < noOfSliderItems; i++) { // Loops through all available images
+                sliderImages[i].style.width = imgWidth; // Sets imgWidth as the width of each image in the slider
+            }
+            sliderContent.style.marginLeft = '-'+n+'00%';
 
+        }
 
-	*/
+        function setAnimation() {
+ 			setTimeout(function() {
+            requestAnimationFrame(setAnimation);
+             // Drawing code goes here
+        	if( n < (noOfSliderItems -1)) {
+        	n++;
+             _render();
+         	}
+            else { 
+            n = 0;
+            _render();
+        	}
+        	
+   			}, 5000);
+        }
+
+    })();
 }
